3a55b6215e7c2d7242e4787937d4948ca388d7bc
Ricky Reed <rreed@officegame.io>
2020-10-29 12:25:37 -0700

    Implemented delete customer

--- customers/ui_customers.py
+++ customers/ui_customers.py
@@ -59,7 +59,49 @@ def call_find_customer():
     pass
 
 def call_delete_customer():
-    pass
+    ui.clear()
+    print('Delete Customer')
+    print()
+
+    customers = []
+    if path.isfile('data/customers.csv'):
+        with open('data/customers.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    print(line_count, end = '')
+                    print(' ' + row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3])
+                    customers.append(row)
+                    line_count += 1
+
+    print()
+    try:
+        menu_selection = int(input('Select customer to delete: '))
+    except:
+        print(ui.choice_invalid_messaging)
+        time.sleep(ui.invalid_timer)
+        return
+
+    fields = ['first_name', 'last_name', 'address', 'email']
+    customers.pop(menu_selection - 1)
+
+    with open("data/customers.csv", 'w+') as csvfile:
+       # creating a csv writer object  
+       csvwriter = csv.writer(csvfile) 
+
+       if csvfile.tell() == 0:
+           # writing the fields  
+           csvwriter.writerow(fields)
+
+       # writing the data rows  
+       csvwriter.writerows(customers)
+
+    ui.clear()
+    print('Customer deleted')
+    time.sleep(ui.invalid_timer)
 
 def check_email(email):
     regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
