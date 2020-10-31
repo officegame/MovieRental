6442231a18cd9ae4fb5572539fbca5741e6b0c14
teejay <thomas.wilform@gmail.com>
2020-10-31 13:33:56 +0000

    Added find feature

--- movies/ui_movies.py
+++ movies/ui_movies.py
@@ -2,6 +2,7 @@ import time
 import ui.main as ui
 import csv
 from os import path
+import re
 
 def call_add_movie():
     ui.clear()
@@ -53,7 +54,33 @@ def call_edit_movie():
     pass
 
 def call_find_movie():
-    pass
+    ui.clear()
+    print('Find Movie')
+    print()
+
+    movie_title = input('Movie Title: ')
+
+    if path.isfile('data/movie.csv'):
+        with open('data/movie.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            found = False
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    if row[0] == movie_title:
+                        print()
+                        print(line_count, end = '')
+                        print(' ' + row[0] + ' ' + row[1] + ' ' + row[2] + ' ' + row[3])
+                        print()
+                        found = True
+                        _ = input('Press enter to continue')
+                    line_count += 1
+    
+    if found == False:
+        print('Movie not found')
+        time.sleep(ui.invalid_timer)
 
 def call_delete_movie():
     pass
